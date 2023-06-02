475a68e1887e6f160893131da410426b8116de7d
Jun Kim <jun7.kim@gmail.com>
2023-06-02 11:49:07 +0900

    Length score 계산 기능 구현

--- Sample-Test1/sc_test.cpp
+++ Sample-Test1/sc_test.cpp
@@ -1,6 +1,10 @@
 #include "pch.h"
 #include "../SimilarityChecker/similarity_checker.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
+TEST(SimilarityChecker, Length) {
+	SimilarityChecker sc;
+	EXPECT_EQ(60, sc.getLengthScore("ASD", "DSA"));
+	EXPECT_EQ(0, sc.getLengthScore("A", "BB"));
+	EXPECT_EQ(20, sc.getLengthScore("AAABB", "BAA"));
+	EXPECT_EQ(30, sc.getLengthScore("AA", "AAA"));
 }
